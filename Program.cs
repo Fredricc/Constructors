53c9539b3ab214a3f7ba24fea5fc9db18c2ef672
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-01-15 14:32:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Constructors
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        //public Vehicle()
+        //{
+        //    Console.WriteLine("Vehicle is being initialized.");
+        //}
+        public Vehicle(string RegistrationNumber)
+        {
+           _registrationNumber = RegistrationNumber;
+            Console.WriteLine($"Vehicle is being initialized. {_registrationNumber}");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+            : base( registrationNumber )
+        {
+            Console.WriteLine($"Car is being initialized. {registrationNumber}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("KBL 149L");
+        }
+    }
+}
